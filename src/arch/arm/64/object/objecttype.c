kent-mcleod/seL4

arm: Add new APIs for generating SGIs
Allow SGIs to be generated from non-SMP kernels.

src/arch/arm/64/object/objecttype.c
@@ -97,6 +97,13 @@ deriveCap_ret_t Arch_deriveCap(cte_t *slot, cap_t cap)
         ret.status = EXCEPTION_NONE;
         return ret;
 #endif
+#if CONFIG_MAX_NUM_NODES == 1
+    case cap_sgi_signal_cap:
+        ret.cap = cap;
+        ret.status = EXCEPTION_NONE;
+        return ret;
+#endif
+
 #ifdef CONFIG_ARM_SMMU
     case cap_sid_control_cap:
     case cap_cb_control_cap:
@@ -213,6 +220,13 @@ finaliseCap_ret_t Arch_finaliseCap(cap_t cap, bool_t final)
         }
         break;
 #endif
+#if CONFIG_MAX_NUM_NODES == 1
+    case cap_sgi_signal_cap:
+        // do nothing
+        break;
+#endif
+
+
 #ifdef CONFIG_ARM_SMMU
     case cap_cb_cap:
         if (cap_cb_cap_get_capBindSID(cap) != SID_INVALID) {
@@ -299,6 +313,17 @@ bool_t CONST Arch_sameRegionAs(cap_t cap_a, cap_t cap_b)
         }
         break;
 #endif
+#if CONFIG_MAX_NUM_NODES == 1
+    case cap_sgi_signal_cap:
+        if (cap_get_capType(cap_b) == cap_sgi_signal_cap) {
+            return (cap_sgi_signal_cap_get_capSGIIRQMask(cap_a) ==
+                    cap_sgi_signal_cap_get_capSGIIRQMask(cap_b) &&
+                    cap_sgi_signal_cap_get_capSGITargetMask(cap_a) ==
+                    cap_sgi_signal_cap_get_capSGITargetMask(cap_b));
+        }
+        break;
+#endif
+
 #ifdef CONFIG_ARM_SMMU
     case cap_sid_control_cap:
         if (cap_get_capType(cap_b) == cap_sid_control_cap ||
@@ -479,12 +504,16 @@ exception_t Arch_decodeInvocation(word_t label, word_t length, cptr_t cptr,
     /* The C parser cannot handle a switch statement with only a default
      * case. So we need to do some gymnastics to remove the switch if
      * there are no other cases */
-#if defined(CONFIG_ARM_HYPERVISOR_SUPPORT) || defined(CONFIG_ARM_SMMU)
+#if defined(CONFIG_ARM_HYPERVISOR_SUPPORT) || defined(CONFIG_ARM_SMMU) || (CONFIG_MAX_NUM_NODES == 1)
     switch (cap_get_capType(cap)) {
 #ifdef CONFIG_ARM_HYPERVISOR_SUPPORT
     case cap_vcpu_cap:
         return decodeARMVCPUInvocation(label, length, cptr, slot, cap, call, buffer);
 #endif /* end of CONFIG_ARM_HYPERVISOR_SUPPORT */
+#if CONFIG_MAX_NUM_NODES == 1
+    case cap_sgi_signal_cap:
+        return decodeSGISignalInvocation(label, length, cap, buffer);
+#endif /* end of CONFIG_MAX_NUM_NODES == 1 */
 #ifdef CONFIG_ARM_SMMU
     case cap_sid_control_cap:
         return decodeARMSIDControlInvocation(label, length, cptr, slot, cap, call, buffer);

src/arch/arm/object/interrupt.c
@@ -18,6 +18,19 @@ static exception_t Arch_invokeIRQControl(irq_t irq, cte_t *handlerSlot, cte_t *c
     return invokeIRQControl(irq, handlerSlot, controlSlot);
 }
 
+
+#if CONFIG_MAX_NUM_NODES == 1
+
+static exception_t Arch_invokeIssueSGISignal(word_t irqs, word_t targets, cte_t *sgiSlot, cte_t *controlSlot)
+{
+
+    cteInsert(cap_sgi_signal_cap_new(irqs, targets), controlSlot, sgiSlot);
+
+    return EXCEPTION_NONE;
+}
+
+#endif
+
 exception_t Arch_decodeIRQControlInvocation(word_t invLabel, word_t length,
                                             cte_t *srcSlot, word_t *buffer)
 {
@@ -77,6 +90,54 @@ exception_t Arch_decodeIRQControlInvocation(word_t invLabel, word_t length,
 
         setThreadState(NODE_STATE(ksCurThread), ThreadState_Restart);
         return Arch_invokeIRQControl(irq, destSlot, srcSlot, trigger);
+#if CONFIG_MAX_NUM_NODES == 1
+    } else if (invLabel == ARMIRQIssueSGISignal) {
+        if (length < 3 || current_extra_caps.excaprefs[0] == NULL) {
+            userError("IRQControl: IssueSGISignal: Truncated message.");
+            current_syscall_error.type = seL4_TruncatedMessage;
+            return EXCEPTION_SYSCALL_ERROR;
+        }
+        word_t irqs = getSyscallArg(0, buffer);
+        word_t targets = getSyscallArg(1, buffer);
+        word_t index = getSyscallArg(2, buffer);
+        word_t depth = getSyscallArg(3, buffer);
+
+        cap_t cnodeCap = current_extra_caps.excaprefs[0]->cap;
+
+        if (irqs > MASK(NUM_SGIS)) {
+            current_syscall_error.type = seL4_RangeError;
+            current_syscall_error.rangeErrorMin = 0;
+            current_syscall_error.rangeErrorMax = MASK(NUM_SGIS);
+            userError("IRQControl: IssueSGISignal: Invalid SGI IRQ mask 0x%lx.", irqs);
+            return EXCEPTION_SYSCALL_ERROR;
+        }
+
+        if (targets > MASK(GIC_SGI_TARGET_MASK_BITS)) {
+            current_syscall_error.type = seL4_RangeError;
+            current_syscall_error.rangeErrorMin = 0;
+            current_syscall_error.rangeErrorMax = MASK(GIC_SGI_TARGET_MASK_BITS);
+            userError("IRQControl: IssueSGISignal: Invalid SGI Target mask 0x%lx.", targets);
+            return EXCEPTION_SYSCALL_ERROR;
+        }
+
+        lookupSlot_ret_t lu_ret = lookupTargetSlot(cnodeCap, index, depth);
+        if (lu_ret.status != EXCEPTION_NONE) {
+            userError("IRQControl: IssueSGISignal: Target slot for new ARM_SGI_Signal cap invalid: cap %lu.",
+                      getExtraCPtr(buffer, 0));
+            return lu_ret.status;
+        }
+        cte_t *destSlot = lu_ret.slot;
+
+        exception_t status = ensureEmptySlot(destSlot);
+        if (status != EXCEPTION_NONE) {
+            userError("IRQControl: IssueSGISignal: Target slot for new ARM_SGI_Signal cap not empty: cap %lu.",
+                      getExtraCPtr(buffer, 0));
+            return status;
+        }
+
+        return Arch_invokeIssueSGISignal(irqs, targets, destSlot, srcSlot);
+
+#endif
 #ifdef ENABLE_SMP_SUPPORT
     } else if (invLabel == ARMIRQIssueIRQHandlerTriggerCore) {
         word_t irq_w = getSyscallArg(0, buffer);
@@ -135,3 +196,70 @@ exception_t Arch_decodeIRQControlInvocation(word_t invLabel, word_t length,
         return EXCEPTION_SYSCALL_ERROR;
     }
 }
+
+#if CONFIG_MAX_NUM_NODES == 1
+
+static exception_t invokeSGISignalGenerate(word_t irq, word_t targets)
+{
+    ipi_send_target(CORE_IRQ_TO_IRQT(0, irq), targets);
+    return EXCEPTION_NONE;
+}
+
+
+exception_t decodeSGISignalInvocation(word_t invLabel, word_t length,
+                                      cap_t cap, word_t *buffer)
+{
+    switch (invLabel) {
+
+    case ARMSGISignalGenerate: {
+        if (length < 2) {
+            userError("SGISignal: Generate: Truncated message.");
+            current_syscall_error.type = seL4_TruncatedMessage;
+            return EXCEPTION_SYSCALL_ERROR;
+        }
+
+        word_t irq = getSyscallArg(0, buffer);
+        word_t targets = getSyscallArg(1, buffer);
+
+        if (irq >= NUM_SGIS) {
+            current_syscall_error.type = seL4_RangeError;
+            current_syscall_error.rangeErrorMin = 0;
+            current_syscall_error.rangeErrorMax = NUM_SGIS-1;
+            userError("SGISignal: Generate: Invalid SGI IRQ %ld.", irq);
+            return EXCEPTION_SYSCALL_ERROR;
+        }
+
+        if (targets > MASK(GIC_SGI_TARGET_MASK_BITS)) {
+            current_syscall_error.type = seL4_RangeError;
+            current_syscall_error.rangeErrorMin = 0;
+            current_syscall_error.rangeErrorMax = MASK(GIC_SGI_TARGET_MASK_BITS);
+            userError("IRQControl: IssueSGISignal: Invalid SGI Target mask 0x%lx.", targets);
+            return EXCEPTION_SYSCALL_ERROR;
+        }
+
+        word_t irqs_auth = cap_sgi_signal_cap_get_capSGIIRQMask(cap);
+        word_t targets_auth = cap_sgi_signal_cap_get_capSGITargetMask(cap);
+
+        if (!(BIT(irq) & irqs_auth)) {
+            current_syscall_error.type = seL4_IllegalOperation;
+            userError("IRQControl: IssueSGISignal: Unauthorized SGI number %ld.", irq);
+            return EXCEPTION_SYSCALL_ERROR;
+        }
+
+        if ((targets & targets_auth) != targets) {
+            current_syscall_error.type = seL4_IllegalOperation;
+            userError("IRQControl: IssueSGISignal: Unauthorized Target mask 0x%lx.", targets);
+            return EXCEPTION_SYSCALL_ERROR;
+        }
+
+        invokeSGISignalGenerate(irq, targets);
+        return EXCEPTION_NONE;
+    }
+
+    default:
+        userError("SGISignal: Illegal operation.");
+        current_syscall_error.type = seL4_IllegalOperation;
+        return EXCEPTION_SYSCALL_ERROR;
+    }
+}
+#endif /* CONFIG_MAX_NUM_NODES == 1 */

mikemospan/seL4-core-mng

src/arch/arm/64/object/objecttype.c
@@ -73,6 +73,13 @@ deriveCap_ret_t Arch_deriveCap(cte_t *slot, cap_t cap)
         ret.status = EXCEPTION_NONE;
         return ret;
 #endif
+#if CONFIG_MAX_NUM_NODES == 1
+    case cap_sgi_signal_cap:
+        ret.cap = cap;
+        ret.status = EXCEPTION_NONE;
+        return ret;
+#endif
+
 #ifdef CONFIG_ARM_SMMU
     case cap_sid_control_cap:
     case cap_cb_control_cap:
@@ -177,6 +184,13 @@ finaliseCap_ret_t Arch_finaliseCap(cap_t cap, bool_t final)
         }
         break;
 #endif
+#if CONFIG_MAX_NUM_NODES == 1
+    case cap_sgi_signal_cap:
+        // do nothing
+        break;
+#endif
+
+
 #ifdef CONFIG_ARM_SMMU
     case cap_cb_cap:
         if (cap_cb_cap_get_capBindSID(cap) != SID_INVALID) {
@@ -249,6 +263,17 @@ bool_t CONST Arch_sameRegionAs(cap_t cap_a, cap_t cap_b)
         }
         break;
 #endif
+#if CONFIG_MAX_NUM_NODES == 1
+    case cap_sgi_signal_cap:
+        if (cap_get_capType(cap_b) == cap_sgi_signal_cap) {
+            return (cap_sgi_signal_cap_get_capSGIIRQ(cap_a) ==
+                    cap_sgi_signal_cap_get_capSGIIRQ(cap_b) &&
+                    cap_sgi_signal_cap_get_capSGITargetMask(cap_a) ==
+                    cap_sgi_signal_cap_get_capSGITargetMask(cap_b));
+        }
+        break;
+#endif
+
 #ifdef CONFIG_ARM_SMMU
     case cap_sid_control_cap:
         if (cap_get_capType(cap_b) == cap_sid_control_cap ||
@@ -457,12 +482,16 @@ exception_t Arch_decodeInvocation(word_t label, word_t length, cptr_t cptr,
     /* The C parser cannot handle a switch statement with only a default
      * case. So we need to do some gymnastics to remove the switch if
      * there are no other cases */
-#if defined(CONFIG_ARM_HYPERVISOR_SUPPORT) || defined(CONFIG_ARM_SMMU) || defined(CONFIG_ALLOW_SMC_CALLS)
+#if defined(CONFIG_ARM_HYPERVISOR_SUPPORT) || defined(CONFIG_ARM_SMMU) || defined(CONFIG_ALLOW_SMC_CALLS) || (CONFIG_MAX_NUM_NODES == 1)
     switch (cap_get_capType(cap)) {
 #ifdef CONFIG_ARM_HYPERVISOR_SUPPORT
     case cap_vcpu_cap:
         return decodeARMVCPUInvocation(label, length, cptr, slot, cap, call, buffer);
 #endif /* end of CONFIG_ARM_HYPERVISOR_SUPPORT */
+#if CONFIG_MAX_NUM_NODES == 1
+    case cap_sgi_signal_cap:
+        return decodeSGISignalInvocation(label, length, cap, buffer);
+#endif /* end of CONFIG_MAX_NUM_NODES == 1 */
 #ifdef CONFIG_ARM_SMMU
     case cap_sid_control_cap:
         return decodeARMSIDControlInvocation(label, length, cptr, slot, cap, call, buffer);

src/arch/arm/object/interrupt.c
@@ -18,6 +18,19 @@ static exception_t Arch_invokeIRQControl(irq_t irq, cte_t *handlerSlot, cte_t *c
     return invokeIRQControl(irq, handlerSlot, controlSlot);
 }
 
+
+#if CONFIG_MAX_NUM_NODES == 1
+
+static exception_t Arch_invokeIssueSGISignal(word_t irq, word_t targets, cte_t *sgiSlot, cte_t *controlSlot)
+{
+
+    cteInsert(cap_sgi_signal_cap_new(irq, targets), controlSlot, sgiSlot);
+
+    return EXCEPTION_NONE;
+}
+
+#endif
+
 exception_t Arch_decodeIRQControlInvocation(word_t invLabel, word_t length,
                                             cte_t *srcSlot, word_t *buffer)
 {
@@ -77,6 +90,54 @@ exception_t Arch_decodeIRQControlInvocation(word_t invLabel, word_t length,
 
         setThreadState(NODE_STATE(ksCurThread), ThreadState_Restart);
         return Arch_invokeIRQControl(irq, destSlot, srcSlot, trigger);
+#if CONFIG_MAX_NUM_NODES == 1
+    } else if (invLabel == ARMIRQIssueSGISignal) {
+        if (length < 3 || current_extra_caps.excaprefs[0] == NULL) {
+            userError("IRQControl: IssueSGISignal: Truncated message.");
+            current_syscall_error.type = seL4_TruncatedMessage;
+            return EXCEPTION_SYSCALL_ERROR;
+        }
+        word_t irq = getSyscallArg(0, buffer);
+        word_t targets = getSyscallArg(1, buffer);
+        word_t index = getSyscallArg(2, buffer);
+        word_t depth = getSyscallArg(3, buffer);
+
+        cap_t cnodeCap = current_extra_caps.excaprefs[0]->cap;
+
+        if (irq >= NUM_SGIS) {
+            current_syscall_error.type = seL4_RangeError;
+            current_syscall_error.rangeErrorMin = 0;
+            current_syscall_error.rangeErrorMax = NUM_SGIS -1;
+            userError("IRQControl: IssueSGISignal: Invalid SGI IRQ 0x%lx.", irq);
+            return EXCEPTION_SYSCALL_ERROR;
+        }
+
+        if (targets > MASK(GIC_SGI_TARGET_MASK_BITS)) {
+            current_syscall_error.type = seL4_RangeError;
+            current_syscall_error.rangeErrorMin = 0;
+            current_syscall_error.rangeErrorMax = MASK(GIC_SGI_TARGET_MASK_BITS);
+            userError("IRQControl: IssueSGISignal: Invalid SGI Target mask 0x%lx.", targets);
+            return EXCEPTION_SYSCALL_ERROR;
+        }
+
+        lookupSlot_ret_t lu_ret = lookupTargetSlot(cnodeCap, index, depth);
+        if (lu_ret.status != EXCEPTION_NONE) {
+            userError("IRQControl: IssueSGISignal: Target slot for new ARM_SGI_Signal cap invalid: cap %lu.",
+                      getExtraCPtr(buffer, 0));
+            return lu_ret.status;
+        }
+        cte_t *destSlot = lu_ret.slot;
+
+        exception_t status = ensureEmptySlot(destSlot);
+        if (status != EXCEPTION_NONE) {
+            userError("IRQControl: IssueSGISignal: Target slot for new ARM_SGI_Signal cap not empty: cap %lu.",
+                      getExtraCPtr(buffer, 0));
+            return status;
+        }
+        setThreadState(NODE_STATE(ksCurThread), ThreadState_Restart);
+        return Arch_invokeIssueSGISignal(irq, targets, destSlot, srcSlot);
+
+#endif
 #ifdef ENABLE_SMP_SUPPORT
     } else if (invLabel == ARMIRQIssueIRQHandlerTriggerCore) {
         word_t irq_w = getSyscallArg(0, buffer);
@@ -135,3 +196,24 @@ exception_t Arch_decodeIRQControlInvocation(word_t invLabel, word_t length,
         return EXCEPTION_SYSCALL_ERROR;
     }
 }
+
+#if CONFIG_MAX_NUM_NODES == 1
+
+static exception_t invokeSGISignalGenerate(word_t irq, word_t targets)
+{
+    ipi_send_target(CORE_IRQ_TO_IRQT(0, irq), targets);
+    return EXCEPTION_NONE;
+}
+
+
+exception_t decodeSGISignalInvocation(word_t invLabel, word_t length,
+                                      cap_t cap, word_t *buffer)
+{
+
+    word_t irq_auth = cap_sgi_signal_cap_get_capSGIIRQ(cap);
+    word_t targets_auth = cap_sgi_signal_cap_get_capSGITargetMask(cap);
+
+    setThreadState(NODE_STATE(ksCurThread), ThreadState_Restart);
+    return invokeSGISignalGenerate(irq_auth, targets_auth);
+}
+#endif /* CONFIG_MAX_NUM_NODES == 1 */